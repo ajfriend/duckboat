8d2f3833cc04affbe0e7477bd54b79b774f67b70
AJ Friend <ajfriend@gmail.com>
2024-12-07 14:39:26 -0700

    get if file

--- src/darkwing/doer.py
+++ src/darkwing/doer.py
@@ -1,4 +1,21 @@
-from ._get_if_file import _get_if_file
+from pathlib import Path
+
+def _is_file(s: str) -> bool:
+    try:
+        return Path(s).is_file()
+    except (OSError, TypeError):
+        # catches `OSError: [Errno 63] File name too long`
+        return False
+
+def _get_if_file(s) -> str:
+    if isinstance(s, Path):
+        s = s.read_text()
+
+    if _is_file(s):
+        with open(s) as f:
+            s = f.read()
+
+    return s
 
 def do(A, *xs):
     for x in xs:
