95e05b22542198da4c541c5bff3b7b541b32a3a7
AJ Friend <ajfriend@gmail.com>
2024-12-22 20:34:34 -0500

    bah

--- src/darkwing/ddb/__init__.py
+++ src/darkwing/ddb/__init__.py
@@ -1,2 +1,3 @@
 from ._query import __darkwing_query__ as query
 from ._relation import form_relation
+from ._con import __darkwing_con__ as con
--- src/darkwing/table.py
+++ src/darkwing/table.py
@@ -2,14 +2,38 @@ from .ddb import form_relation
 from .mixin_do import DoMixin
 from .mixin_table import TableMixin
 
+from duckdb import DuckDBPyRelation
+
+
+# IDEA: maybe a `defer_repr` flag on tables that infects downstream tables and databases?
+# `defer_repr` is only relevant in the repr.
+# actually, maybe we don't need it to infect. we can just pop it in when needed.
+# repr = False
+
+# we should probably cache the repr output, assume all table objects are immutable.
+# we need to figure out how to handle the instance variables, since those will also be represented
+# i think we can simplify to have table just have `rel` and no `raw`.
+# if people want dataframes, they can get a dict of dfs from `database.df()) or something, but this can be utilized in a do sequence if they wanted to keep going
+# if they want to see what a dict of dataframes looks like, they put it into a `Database(**d)`
+# repr=False might need to pass down to child Tables
+
 class Table(TableMixin, DoMixin):
     """
     The table name is always included implicitly when applying a SQL snippet.
     """
-    def __init__(self, other):
+    raw: object
+    rel: DuckDBPyRelation
+    defer_repr: bool
+
+    def __init__(self, other, defer_repr=False):
+        self.defer_repr = defer_repr
+
         if isinstance(other, Table):
             self.raw = other.raw
             self.rel = other.rel
+        elif isinstance(other, DuckDBPyRelation):
+            self.raw = other
+            self.rel = other
         else:
             self.raw = other
             self.rel = form_relation(other)
