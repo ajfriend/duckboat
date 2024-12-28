153f0e236ded7203c2ca45b7d37e27cc8687e996
AJ Friend <ajfriend@gmail.com>
2024-12-28 18:28:26 -0500

    repr tests

---
+++ tests/test_repr.py
@@ -0,0 +1,42 @@
+import duckboat as uck
+import pandas as pd
+
+
+def test_repr():
+    from textwrap import dedent
+
+    df = pd.DataFrame({'a': [0]})
+    out = """
+    ┌───────┐
+    │   a   │
+    │ int64 │
+    ├───────┤
+    │     0 │
+    └───────┘
+    """
+    out = dedent(out)
+    out = out.strip()
+    out += '\n'
+
+
+    t = uck.Table(df)
+    assert repr(t) == out
+
+    t = uck.Table(df, _hide=False)
+    assert repr(t) == out
+
+    t = uck.Table(df, _hide=True)
+    assert repr(t) == '<Table(..., _hide=True)>'
+
+
+def test_rowcols():
+    import numpy as np
+    shape = (17, 4)
+
+    df = pd.DataFrame(np.zeros(shape))
+    t = uck.Table(df)
+
+    assert t.rowcols() == "17 x ['0', '1', '2', '3']"
+
+    t = uck.Table(df).hide()
+    assert t.rowcols() == '<Table(..., _hide=True)>'
